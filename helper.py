ab928bb6a0162488e1f1eef885f5592e30c12dcc
Nicola Colic <Nicola.Colic@bbbaden.ch>
2023-08-15 14:55:06 +0200

    musterlösung 1601

--- helper.py
+++ helper.py
@@ -1,26 +1,35 @@
 from dataclasses import dataclass
 
-items = []
+# Exercise 1602: This is where the data is stored:
+# It's just a simple array, which we will replace
+# by a database at a later point
+todos = []
 
 
 @dataclass
-class Item:
-    text: str
+class Todo:
+    title: str
     isCompleted: bool = False
 
 
-def add(text):
-    text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
+def add(title):
+    # Exercise 1602: This is where the bbb-isation happens
+    title = title.replace('b', 'bbb').replace('B', 'Bbb')
+    todos.append(Todo(title))
 
 
 def get_all():
-    return items
+    """This actually isn't necessary, in python you don't have 
+    to deal with things such as private and public
+    and getters and setters if you don't want to. You can access 
+    todos directly from main.py"""
+    return todos
 
 
 def get(index):
-    return items[index]
+    """Same as above"""
+    return todos[index]
 
 
 def update(index):
-    items[index].isCompleted = not items[index].isCompleted
+    todos[index].isCompleted = not todos[index].isCompleted
--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import helper
 from flask import Flask, request, Response, render_template, redirect, url_for
 app = Flask(__name__)
 
+# Exercise 1602: With these decorators (code that starts with @)
+# we can define the URLS that the app listens to.
+
+
 @app.route("/")
 def index():
-    items = helper.get_all()
-    return render_template('index.html', items=items)
+    # see comment in helper.py about getters and setters
+    todos = helper.todos
+    # Exercise 1602: Here we pass the data to the HTML
+    # Normally this isn't possible, but flask will use this data
+    # to generate new HTMLs to send to the client
+    return render_template('index.html', todos=todos)
 
 
-@app.route('/add', methods=["POST"])
+@app.route('/create/')
 def add():
-    text = request.form.get("text")
-    helper.add(text)
+    title = request.args.get("title")
+    print(title)
+    helper.add(title)
     return redirect(url_for("index"))
 
 
@@ -19,3 +28,11 @@ def add():
 def update(index):
     helper.update(index)
     return redirect(url_for("index"))
+
+
+# Exercise 1602: With these decorators (code that starts with @)
+# we can define the URLS that the app listens to. Here we defined
+# a new one!
+@app.route('/secret')
+def secret():
+    return 42

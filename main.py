6ad1cc2ed5ef5d5d8854f5bc0ebe888e4b243127
Dominik Kreiselmaier <D.Kreiselmaier.inf21@stud.bbbaden.ch>
2023-08-25 14:23:17 +0200

    add .gitignore

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import helper
 from flask import Flask, request, Response, render_template, redirect, url_for
 app = Flask(__name__)
 
+#urls regieren
 @app.route("/")
 def index():
     items = helper.get_all()

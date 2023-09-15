85dc26c8270e6e7bd869cd49aaebaeccd8449a0b
Dominik Kreiselmaier <D.Kreiselmaier.inf21@stud.bbbaden.ch>
2023-09-15 14:58:21 +0200

    add csv feature  #17

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ import helper
 app = Flask(__name__)
 
 
+@app.route("/getCSV")
+def get_csv():
+    return Response(
+        helper.get_csv(),
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=zu-bbbearbeiten.csv"},
+    )
+
+
 @app.route("/")
 def index():
-    todos = helper.get_all()
-    return render_template("index.html", todos=todos)
+    items = helper.get_all()
+    return render_template("index.html", items=items)
 
 
 @app.route("/add", methods=["POST"])
 def add():
-    title = request.form.get("text")
-    date = request.form.get("date")
+    text = request.form.get("text")
+    date = request.form.get("deadline")
     category = request.form.get("category")
     description = request.form.get("description")
-    helper.add(title, date, category, description)
+    helper.add(text, date=date, category=category, description=description)
     return redirect(url_for("index"))
 
 
